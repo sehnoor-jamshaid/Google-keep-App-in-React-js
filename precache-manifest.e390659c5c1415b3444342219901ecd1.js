efa07644c4bcb294968597eaf23c03acd292937b
sehnoor-jamshaid <sehnoor.jam@gmail.com>
2020-10-20 17:18:00 +0500

    Updates

---
+++ precache-manifest.e390659c5c1415b3444342219901ecd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd5eb3aa2040d8f67f8e79e92f552fa",
+    "url": "/googlekeepapp/index.html"
+  },
+  {
+    "revision": "5df4fb035c44e9a75b4a",
+    "url": "/googlekeepapp/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f5aa84f62198ad68a06d",
+    "url": "/googlekeepapp/static/css/main.6e39dcf5.chunk.css"
+  },
+  {
+    "revision": "5df4fb035c44e9a75b4a",
+    "url": "/googlekeepapp/static/js/2.e49e5668.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/googlekeepapp/static/js/2.e49e5668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5aa84f62198ad68a06d",
+    "url": "/googlekeepapp/static/js/main.b0511dae.chunk.js"
+  },
+  {
+    "revision": "c9c40dd5030fadc5351d",
+    "url": "/googlekeepapp/static/js/runtime-main.9fa0649b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/googlekeepapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
